576d051419945d05267ba5f3ea94388f64d40ea5
Marvin Becker <marvinbecker@derwebcoder.de>
2025-02-15 15:05:49 +0100

    Upgrade vitest 2 > 3

--- packages/dependency-injection/vite.config.ts
+++ packages/dependency-injection/vite.config.ts
@@ -6,10 +6,9 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  // we ignore this error for now until vitest 3.0.0 is out
-  // see https://github.com/vitest-dev/vitest/discussions/7075#discussioncomment-11541298
   test: {
     include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
+    environment: "jsdom",
     globals: true,
   },
   plugins: [tsconfigPaths()],
