6cb284419f2f382690d81a8b07711f7c60ad90d9
Marvin Becker <marvinbecker@derwebcoder.de>
2025-07-21 22:23:48 +0200

    Upgrade electron 34.2 -> 37.2 plus @electron-forge and run audit fix

--- apps/desktop/src/preload.ts
+++ apps/desktop/src/preload.ts
@@ -6,6 +6,12 @@ import { SharedSettings, WebAppBridge } from "@neote/shared";
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 const { contextBridge } = require("electron");
 
+// contextBridge.exposeInMainWorld('versions', {
+//   node: () => process.versions.node,
+//   chrome: () => process.versions.chrome,
+//   electron: () => process.versions.electron
+// })
+
 contextBridge.exposeInMainWorld("neote", {
   isApp: true,
   window: {
